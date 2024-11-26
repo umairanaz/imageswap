81f28c0366e87302746c293ec82f7746aa6eb33d
umairanaz <umairanaz668@gmail.com>
2024-11-27 02:34:05 +0500

    clickbutton

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    
+    src1=img1.src;
+    src2=img2.src;
+    img1.src=src2;
+    img2.src=src1
+
+
+});
